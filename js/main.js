efd880cd1abdfa14086052f639606579980f5d5b
VerNiBE <nico-verv@hotmail.com>
2019-01-23 16:17:41 +0100

    modif 230119 - 2
    media queries
    
    burger menu

---
+++ js/main.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    document.getElementById("icon_burger").addEventListener('click',function(){
+        document.getElementById("icon_burger").style.display="none";
+        document.getElementById("menuLink").style.display="block";
+        
+
+    });
+}
