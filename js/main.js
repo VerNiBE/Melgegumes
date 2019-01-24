490e7faf4eb26f109b79ad8618e52e1ef5db67cf
VerNiBE <nico-verv@hotmail.com>
2019-01-24 17:09:36 +0100

    Reset
    clean css

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 window.onload = function () {
     document.getElementById("icon_burger").addEventListener('click',function(){
-        document.getElementById("icon_burger").style.display="none";
-        document.getElementById("menuLink").style.display="block";
+        // document.getElementById("icon_burger").style.display="block";
+        console.log(1);
+        
+
+                var menu = document.getElementById("menuLink");
+                menu.classList.toggle("visible");
+
+
+
+
+        console.log(2);
+        
+
         
 
     });
