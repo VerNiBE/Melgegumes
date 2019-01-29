cf51d3e175d35c2b71b70defdb39f07bdc89b903
VerNiBE <nico-verv@hotmail.com>
2019-01-29 17:19:41 +0100

    update 290119-02
    menu burger
    
    responsive etap 1

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 window.onload = function () {
+    console.log(document.getElementById("icon_burger").display=="block");
+                    
     document.getElementById("icon_burger").addEventListener('click',function(){
         // document.getElementById("icon_burger").style.display="block";
         console.log(1);
         
 
-                var menu = document.getElementById("menuLink");
+                var menu = document.getElementById("nav");
                 menu.classList.toggle("visible");
+                console.log(document.getElementById("icon_burger").display=="none");
 
 
 
@@ -16,4 +19,16 @@ window.onload = function () {
         
 
     });
+    if(document.window.innerWidth >= 768){
+        menu.classList.remove("visible");
+
+        console.log(document.getElementById("icon_burger").display=="block");
+        
+    }
+    for (var i = 0; i < 20; i++) {
+        console.log(i);
+    }
+    console.log(i);
+    
+    
 }
