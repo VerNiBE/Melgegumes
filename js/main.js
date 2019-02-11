a7d5c60d5f11687b9dc471a7aad46ba3d98008f2
NicolasVervloesem <nico-verv@hotmail.com>
2019-02-11 11:24:57 +0100

    Menu Top Fixed 110219 -01
    section : before >> partie base
    
    1 partie mobile
    2 partie tablette

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-window.onload = function () {           
+window.onload = function () {        
+
+    //Script activation du menu burger.
+
     document.getElementById("icon_burger").addEventListener('click',function(){
 
         
@@ -9,4 +12,37 @@ window.onload = function () {
 
     });
     
+    // //Script scroll des link-anchors
+
+    // function anchorScroll(fragment) {
+    //     "use strict";
+    //     var amount, ttarget;
+    //     amount = $('header').height();
+    //     ttarget = $('#' + fragment);
+    //     $('html,body').animate({ scrollTop: ttarget.offset().top - amount }, 250);
+    //     return false;
+    // }
+    
+    // function outsideToHash() {
+    //     "use strict";
+    //     var fragment;
+    //     if (window.location.hash) {
+    //         fragment = window.location.hash.substring(1);
+    //         anchorScroll(fragment);
+    //     }
+    // }
+    
+    // function insideToHash(nnode) {
+    //     "use strict";
+    //     var fragment;
+    //     fragment = $(nnode).attr('href').substring(1);
+    //     anchorScroll(fragment);
+    // }
+    
+    // $(document).ready(function () {
+    //     "use strict";
+    //     $("a[href^='#']").bind('click',  function () {insideToHash(this); });
+    //     outsideToHash();
+    // });
+
 }
