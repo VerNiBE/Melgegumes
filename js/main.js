94df7899bec9d6851483a1e5a71d37cd12e2620a
NicolasVervloesem <nico-verv@hotmail.com>
2019-02-13 08:49:30 +0100

    modif 130219-01

--- js/main.js
+++ js/main.js
@@ -6,43 +6,21 @@ window.onload = function () {
 
         
 
-                var menu = document.getElementById("nav");
-                menu.classList.toggle("visible");
-                console.log(document.getElementById("icon_burger").display=="none");
+                document.getElementById("nav").classList.toggle("visible");
+                document.getElementById("backdrop").classList.toggle("visible");
 
+                console.log(document.getElementById("icon_burger").display=="none");
+    
     });
     
-    // //Script scroll des link-anchors
+    document.getElementById("backdrop").addEventListener('click',function(){
 
-    // function anchorScroll(fragment) {
-    //     "use strict";
-    //     var amount, ttarget;
-    //     amount = $('header').height();
-    //     ttarget = $('#' + fragment);
-    //     $('html,body').animate({ scrollTop: ttarget.offset().top - amount }, 250);
-    //     return false;
-    // }
-    
-    // function outsideToHash() {
-    //     "use strict";
-    //     var fragment;
-    //     if (window.location.hash) {
-    //         fragment = window.location.hash.substring(1);
-    //         anchorScroll(fragment);
-    //     }
-    // }
-    
-    // function insideToHash(nnode) {
-    //     "use strict";
-    //     var fragment;
-    //     fragment = $(nnode).attr('href').substring(1);
-    //     anchorScroll(fragment);
-    // }
-    
-    // $(document).ready(function () {
-    //     "use strict";
-    //     $("a[href^='#']").bind('click',  function () {insideToHash(this); });
-    //     outsideToHash();
-    // });
+        
+
+        document.getElementById("nav").classList.toggle("visible");
+        document.getElementById("backdrop").classList.toggle("visible");
+        console.log(document.getElementById("icon_burger").display=="none");
+
+});
 
 }
