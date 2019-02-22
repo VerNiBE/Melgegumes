79acd32a02f35c4592fd8ab87a99ee7f4b4d5e23
NicolasVervloesem <nico-verv@hotmail.com>
2019-02-22 17:03:39 +0100

    modif 220219-03
    listPartners > flex
    
    position lignStart h1 >mobile et desktop ok
    
    image ok

--- js/main.js
+++ js/main.js
@@ -23,4 +23,14 @@ window.onload = function () {
 
 });
 
+
+// var map = L.map('mapid').setView([50.47011,3.95655], 19);
+
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
+
+// L.marker([50.47011,3.95655]).addTo(map)
+//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//     .openPopup();
 }
