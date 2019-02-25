f7eb5f2ba7989ce15b91212b194dc57a2ab0844d
NicolasVervloesem <nico-verv@hotmail.com>
2019-02-25 11:02:48 +0100

    modif 250219-01
    fixe container
    
    command form graphisme

--- js/main.js
+++ js/main.js
@@ -21,16 +21,9 @@ window.onload = function () {
         document.getElementById("backdrop").classList.toggle("visible");
         console.log(document.getElementById("icon_burger").display=="none");
 
+
 });
 
 
-// var map = L.map('mapid').setView([50.47011,3.95655], 19);
 
-// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// }).addTo(map);
-
-// L.marker([50.47011,3.95655]).addTo(map)
-//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-//     .openPopup();
 }
