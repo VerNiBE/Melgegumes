b1e100a3cc56d886c39972826a5a73a9fe419730
NicolasVervloesem <nico-verv@hotmail.com>
2019-02-28 14:31:52 +0100

    Modif 280219-02
    Js update
    
    input radio maque et reveal la liste de legume
    
    bricolage des first section sur les pages secondaires pour faire apparaitre les noms

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 window.onload = function () {        
 
     //Script activation du menu burger.
-
+    
     document.getElementById("icon_burger").addEventListener('click',function(){
 
         
@@ -11,6 +11,8 @@ window.onload = function () {
     
     });
     
+    //Activation de la zone sombre du menu
+
     document.getElementById("backdrop").addEventListener('click',function(){
 
         
@@ -19,14 +21,20 @@ window.onload = function () {
         document.getElementById("backdrop").classList.toggle("visible");    
     });
 
-    document.getElementsByClassName("linksMenu").addEventListener('click',function(){
+    // Fermeture du menu avec les liens
 
+    var x = document.getElementsByClassName("linksMenu");
+    for (let i = 0; i < x.length; i++) {
         
+        x[i].addEventListener('click',function(){
 
-        document.getElementById("nav").classList.toggle("visible");
-        document.getElementById("backdrop").classList.toggle("visible");
-
-});
+            document.getElementById("nav").classList.toggle("visible");
+            document.getElementById("backdrop").classList.toggle("visible");
+        });
+        
+    }
+    
+    //Activation du panier
 
     document.getElementById("basket").addEventListener('click',function(){
 
@@ -35,6 +43,8 @@ window.onload = function () {
 
     });
 
+    //Activation de la zone sombre du formulaire
+
     document.getElementById("backdropForm").addEventListener('click',function(){
 
         document.getElementById("formulaire").classList.toggle("visible");
@@ -42,5 +52,35 @@ window.onload = function () {
 
     });
 
+    // Liste de choix des paniers
+
+    document.getElementById("random").addEventListener('click',function(e){
+
+        document.getElementById("containerLegumes").classList.remove("invisible");
+        document.getElementById("footer").classList.remove("marginSpace");
+        console.log("checked");
+        
+    });
+
+    document.getElementById("big").addEventListener('click',function(e){
+
+        document.getElementById("containerLegumes").classList.add("invisible");
+        document.getElementById("footer").classList.add("marginSpace");
+        console.log("checked-big");
+    });
+
+    document.getElementById("normal").addEventListener('click',function(e){
+
+        document.getElementById("containerLegumes").classList.add("invisible");
+        document.getElementById("footer").classList.add("marginSpace");
+        console.log("checked-normal");
+    });
+
+    document.getElementById("small").addEventListener('click',function(e){
+
+        document.getElementById("containerLegumes").classList.add("invisible");
+        document.getElementById("footer").classList.add("marginSpace");
+        console.log("checked-small");
+    });
 
 }
